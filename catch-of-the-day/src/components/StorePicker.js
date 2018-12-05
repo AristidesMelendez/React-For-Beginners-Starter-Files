ed02afaefe90f22b9da7802b78db3b0cca899be2
AristidesMelendez <aristides.melendez@admios-sa.com>
2018-12-05 14:48:23 -0500

    Setting up a form.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component{
   render() {
-    return <p>¡Hola!</p>
+    return (
+      // { /* outsite comment !!!! */ }
+      <form className="store-selector">
+        { /* a comment inside html */ }
+        <h3>Please enter a store</h3>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store ➤</button>
+      </form>
+    )
   }
 }
 
